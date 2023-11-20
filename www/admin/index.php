7b819d430a07f976f39aca33063047e044aea681
wilfried <wilfried.thoumire@dataimpact.io>
2023-11-20 15:34:52 +0100

    index

---
+++ www/admin/index.php
@@ -0,0 +1,4 @@
+<?php include ("../inc/header.php");?>
+    <h1>Bienvenue sur ce earthBlog</h1>
+
+<?php include ("../inc/footer.php"); ?>
--- www/index.php
+++ www/index.php
@@ -1,1 +1,4 @@
-<?php
+<?php require ("./inc/header.php");?>
+<h1>Bienvenue sur ce earthBlog</h1>
+
+<?php require ("./inc/footer.php"); ?>
